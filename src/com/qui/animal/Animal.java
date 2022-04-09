83e35a37633f9ca9faca587b9c7bdfe877faf9c4
Hieu Minh Le <hieu.minhle@outlook.com>
2022-04-09 17:05:33 +0700

    Java OOP

---
+++ src/com/qui/animal/Animal.java
@@ -0,0 +1,13 @@
+package com.qui.animal;
+
+/* Class */
+/* How to declare a Java class: access modifier + "class" keyword + class name + {} */
+public class Animal {
+  private String name = "Animal";
+
+  /* Method */
+  /* How to declare a Java method: access modifier + return data type + method name + (method parameters) + {} */
+  private void sayHello() {
+    System.out.println("Hello! I am Animal.");
+  }
+}
---
+++ src/com/qui/animal/Fish.java
@@ -0,0 +1,4 @@
+package com.qui.animal;
+
+public class Fish {
+}
